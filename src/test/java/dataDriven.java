0ba41059aea124ae5279272d9fa59d20c501687d
mfron28 <madalina_fron@yahoo.com>
2024-11-25 15:02:30 +0200

    first commit

---
+++ src/test/java/dataDriven.java
@@ -0,0 +1,38 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataDriven {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis = new FileInputStream("C://Users//mfron//AutomationWorkshopStaffs//demoData.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		int sheets = workbook.getNumberOfSheets();
+		for (int i = 0; i < sheets; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase("testdata")) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				Iterator<Row> rows = sheet.iterator();// sheet is collection of rows
+				Row firstrow = rows.next();
+				Iterator<Cell> cell = firstrow.cellIterator();// row is collection of cells
+				cell.next();
+				int k = 0;
+				int column = 0;
+				while (cell.hasNext()) {
+					Cell value = cell.next();
+					if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+						column = k;
+					}
+					k++;
+				}
+				System.out.println(column);
+			}
+		}
+	}
+
+}
